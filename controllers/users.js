6a8fbc2365275c6c085a6ddfc5636be6c4c014bc
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-07-21 17:15:59 +0300

    sameSite: true

--- controllers/users.js
+++ controllers/users.js
@@ -34,8 +34,7 @@ module.exports.login = (req, res, next) => {
       res.status(200).cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'none',
-        secure: true,
+        sameSite: true,
       }).send({ email });
     })
     .catch(next);
