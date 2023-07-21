1dcd505f58ca7168e3f62276535f4a8f306d1a36
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-07-21 16:17:07 +0300

    Переписан мидлвер CORS

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const helmet = require('helmet');
 
-const cors = require('cors');
+// const cors = require('cors');
 const { celebrate, errors } = require('celebrate');
+const cors = require('../middlewares/cors');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
 
 const { login, createUser, exit } = require('../controllers/users');
@@ -11,22 +12,24 @@ const { rateLimiter } = require('../middlewares/rateLimiter');
 const centralizedErrorHandler = require('../middlewares/centralizedErrorHandler');
 const NotFoundError = require('../utils/errors/not-found-err');
 
-const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3006',
-  'http://front.diploma.anstpov.nomoreparties.sbs',
-  'https://front.diploma.anstpov.nomoreparties.sbs',
-];
+// const allowedCors = [
+//   'http://localhost:3000',
+//   'http://localhost:3006',
+//   'http://front.diploma.anstpov.nomoreparties.sbs',
+//   'https://front.diploma.anstpov.nomoreparties.sbs',
+// ];
 
 const { JoiBodyEmailPassword, JoiBodyEmailPasswordName } = require('../utils/validationConstants');
 
 router.use(helmet()); // Helmet helps secure Express apps by setting HTTP response headers.
 router.use(rateLimiter); // Use to limit repeated requests to public APIs and/or endpoints
 
-router.use(cors({
-  origin: allowedCors,
-  credentials: true,
-})); // подключаем CORS
+// router.use(cors({
+//   origin: allowedCors,
+//   credentials: true,
+// })); // подключаем CORS
+
+router.use(cors);
 
 router.use(requestLogger); // подключаем логгер запросов
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,9 @@
 module.exports = Object.freeze({
   urlRegular: /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/,
+  allowedCors: [
+    'http://localhost:3000',
+    'http://localhost:3006',
+    'http://front.diploma.anstpov.nomoreparties.sbs',
+    'https://front.diploma.anstpov.nomoreparties.sbs',
+  ],
 });
