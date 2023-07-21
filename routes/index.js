4a6bc2760645d8b8043d18aba3df9624c1faaebd
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-07-21 16:45:53 +0300

    Удалены комментарии

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const helmet = require('helmet');
 
-// const cors = require('cors');
 const { celebrate, errors } = require('celebrate');
 const cors = require('../middlewares/cors');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
@@ -12,22 +11,10 @@ const { rateLimiter } = require('../middlewares/rateLimiter');
 const centralizedErrorHandler = require('../middlewares/centralizedErrorHandler');
 const NotFoundError = require('../utils/errors/not-found-err');
 
-// const allowedCors = [
-//   'http://localhost:3000',
-//   'http://localhost:3006',
-//   'http://front.diploma.anstpov.nomoreparties.sbs',
-//   'https://front.diploma.anstpov.nomoreparties.sbs',
-// ];
-
 const { JoiBodyEmailPassword, JoiBodyEmailPasswordName } = require('../utils/validationConstants');
 
 router.use(helmet()); // Helmet helps secure Express apps by setting HTTP response headers.
 router.use(rateLimiter); // Use to limit repeated requests to public APIs and/or endpoints
-
-// router.use(cors({
-//   origin: allowedCors,
-//   credentials: true,
-// })); // подключаем CORS
 
 router.use(cors);
 
