e7daf2cfa0389a9b5949734a44ca7f3461ce8808
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-07-24 18:15:44 +0300

    sameSite: 'none'

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,7 @@ module.exports.login = (req, res, next) => {
       res.status(200).cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'Lax',
+        sameSite: 'none',
         secure: true,
         domain: '.diploma.anstpov.nomoreparties.sbs',
       }).send({ email });
